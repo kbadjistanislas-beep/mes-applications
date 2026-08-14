042ea44ad97681ee2623a65889b06aa5cd53f1d7
kbadjistanislas-beep <kbadjistanislas@gmail.com>
2026-08-13 15:38:29 -0900

    Ajout de lecteur de pdfet suivis des modifications

--- calculatrice.py
+++ calculatrice.py
@@ -1,26 +1,45 @@
 import streamlit as st
 
-if 'historique' not in st.session_state:
-    st.session_state.h = []
 st.set_page_config(page_title="Calculatrice" , page_icon="🧮" , layout="wide" )
+if "calcul" not in st.session_state:
+        st.session_state.calcul = ""
 st.title("CALCULATRICE")
-
-a = st.number_input("Nombre 1", value = 0.0 )
-b = st.number_input("Nombre 2", value = 0.0 )
-choix = st.selectbox("Operations" , ["+","-","*","/"])
-buton = st.button("=")
-if buton :
-            if choix == "+":
-                    st.write(f"resultat:{a+b}")
-            elif choix == "-":
-                    st.write(f"resultat:{a-b}")
-            elif choix == "*":
-                    st.write(f"resultat:{a*b}")
-            elif choix == "/":
-                    if b == 0:
-                            st.write("operation impossible")
-                    else: 
-                        st.write(f"resultat:{round(a/b,3)}")
-else:
-    st.write("operation non chargee")
-        
+st.text_input("Ecran", st.session_state.calcul, disabled=True)
+def bouton(valeur):
+        st.sesion_state.calcul += valeur 
+coln1,coln2,coln3,coln4 =st.columns(4)
+with coln1:
+        if st.button("7"):bouton("7")
+        if st.button("4"):bouton("4")
+        if st.button("1"):bouton("1")
+        if st.button("0"):bouton("0")
+with coln2:
+        if st.button("8"):bouton("8")
+        if st.button("5"):bouton("5")
+        if st.button("2"):bouton("2")
+        if st.button("."):bouton(".")
+with coln3:
+        if st.button("9"):bouton("9")
+        if st.button("6"):bouton("6")
+        if st.button("3"):bouton("3")
+        if st.button("+"):bouton("+")
+with coln4:
+        if st.button("/"):bouton("/")
+        if st.button("*"):bouton("*")
+        if st.button("-"):bouton("-")
+        if st.button("4"):
+                try:
+                        resultat = eval(st.session_state.calcul)
+                        st.session_state.calcul = str(resultat)
+                        st.rerun()
+                except:
+                        st.session_state.calcul = "Erreur"
+                        st.rerun()
+if st.button("AC"):
+        st.session_state.calcul = ""
+        st.rerun()
+st.subheader("fonctions scientifiques")
+c1,c2,c3,c4,c5 = st.columns(5)
+with c1:
+        if st.button():
+                s
--- readpdf.py
+++ readpdf.py
@@ -1,3 +1,19 @@
 import streamlit as st
-with open():
-    jam = st.che
+import flitz 
+st.title("Analyseur PDF")
+with st.form("upload"):
+    pdf = st.file_uploader("Choisir un PDF", type="pdf")
+    uploader = st.form_submit_button("Upload")
+if uploader and pdf :
+    with open("document.pdf","wb") as f:
+        f.write(pdf.getvalue())
+    docu = flitz.open("document.pdf")
+    st.success("PDF importer ")
+    st.writte(f"{len(docu)} pages")
+
+    st.subheader("Debut")
+    texte_debut = docu[0].get_text()
+    st.write(texte_debut[:1000])
+    st.subheader("Fin")
+    texte_fin = docu[-1].get_text()
+    st.write(texte_fin[:1000])
